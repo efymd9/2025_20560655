5674f9e924821f6b91a873ef3b3d5778118ab9f4
efymd9 <efymd9@nottingham.ac.uk>
2026-02-11 23:29:10 +0000

    Worksheet 5 Exercise 1 - Doxygen

---
+++ Worksheet5/adder.h
@@ -0,0 +1,31 @@
+// adder.h
+#ifndef MATHSLIB_ADDER_H
+#define MATHSLIB_ADDER_H
+
+/** @file
+ * This file contains the declarations of all exported functions in the maths library.
+ */
+
+#if defined(WIN32) | defined(_WIN32)
+    #ifdef maths_STATIC
+        #define MATHSLIB_API
+    #else
+        #ifdef maths_EXPORTS
+            #define MATHSLIB_API __declspec(dllexport)
+        #else
+            #define MATHSLIB_API __declspec(dllimport)
+        #endif
+    #endif
+#else
+    #define MATHSLIB_API
+#endif
+
+/** Adding function
+ * This function adds two numbers together
+ * @param a is the first number
+ * @param b is the second number
+ * @return sum of a and b
+ */
+MATHSLIB_API int add(int a, int b);
+
+#endif
